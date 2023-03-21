8915a04b64f820abdd39c035c3efb66a191d088a
Jaesun Park <41979078+ppakjae@users.noreply.github.com>
2023-03-21 09:14:46 +0900

    initial webpage setting

---
+++ app.py
@@ -0,0 +1,64 @@
+import os
+import json
+import tempfile
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
+import subprocess
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "your-secret-key"
+
+MYTHRIL_PATH = "/path/to/mythril"  # Update this with your Mythril path
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        file = request.files.get("file")
+        code = request.form.get("code")
+
+        if file:
+            filename = secure_filename(file.filename)
+            _, file_ext = os.path.splitext(filename)
+
+            if file_ext != ".sol":
+                flash("Please upload a Solidity (.sol) file.")
+                return render_template("index.html")
+
+            temp_dir = tempfile.mkdtemp()
+            file.save(os.path.join(temp_dir, filename))
+            file_path = os.path.join(temp_dir, filename)
+
+        elif code:
+            temp_file = tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".sol")
+            temp_file.write(code)
+            temp_file.close()
+            file_path = temp_file.name
+
+        else:
+            flash("Please provide a Solidity file or enter the code.")
+            return render_template("index.html")
+
+        mythril_cli = "/home/ppakjae/Downloads/mythril-0.23.17/myth"
+        
+        try:
+            mythril_output = subprocess.check_output(
+                [mythril_cli, "analyze", file_path],
+                stderr=subprocess.PIPE,
+            ).decode("utf-8")
+        except subprocess.CalledProcessError as e:
+            mythril_output = e.output.decode("utf-8")
+
+        mythril_json = None
+        if mythril_output:
+            try:
+                mythril_json = json.loads(mythril_output)
+            except json.JSONDecodeError as json_error:
+                print(f"Failed to parse JSON: {json_error}")
+                # flash("An error occurred while analyzing the Solidity code. Please try again.")
+
+        return render_template("index.html", mythril_output=mythril_output, mythril_json=mythril_json)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
