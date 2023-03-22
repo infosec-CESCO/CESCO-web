53216bb866407e1ef9733c395e3d87858715360b
hiro <kyy980708@gmail.com>
2023-03-22 09:30:27 +0900

    분석 모드 추가 및 css입히기

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def index():
     if request.method == "POST":
         file = request.files.get("file")
         code = request.form.get("code")
+        mode = request.form["mode"]
 
         if file:
             filename = secure_filename(file.filename)
@@ -29,7 +30,7 @@ def index():
             file_path = os.path.join(temp_dir, filename)
 
         elif code:
-            temp_file = tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".sol")
+            temp_file = tempfile.NamedTemporaryFile(mode="w", dget_flashed_messageselete=False, suffix=".sol")
             temp_file.write(code)
             temp_file.close()
             file_path = temp_file.name
@@ -38,13 +39,23 @@ def index():
             flash("Please provide a Solidity file or enter the code.")
             return render_template("index.html")
 
-        mythril_cli = "/home/ppakjae/Downloads/mythril-0.23.17/myth"
+        # mythril_cli = "/home/ppakjae/Downloads/mythril-0.23.17/myth"
+        mythril_cli = "/home/infosec/Desktop/mythril-develop/myth"
         
         try:
-            mythril_output = subprocess.check_output(
-                [mythril_cli, "analyze", file_path],
-                stderr=subprocess.PIPE,
-            ).decode("utf-8")
+            # 모드 변경은 여기서 하면 될듯 
+            # switch 문을 통해 disassemble, analyze 가능할듯
+            if mode == "analyze":
+                mythril_output = subprocess.check_output(
+                    [mythril_cli, "analyze", file_path],
+                    stderr=subprocess.PIPE,
+                ).decode("utf-8")
+            elif mode == "disassemble":
+                mythril_output = subprocess.check_output(
+                    [mythril_cli, "disassemble", file_path],
+                    stderr=subprocess.PIPE,
+                ).decode("utf-8")
+
         except subprocess.CalledProcessError as e:
             mythril_output = e.output.decode("utf-8")
 
