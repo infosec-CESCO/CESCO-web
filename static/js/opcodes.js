5315a9491fd61cb692852d869ce235851552427f
hiro <kyy980708@gmail.com>
2023-04-12 09:42:08 +0900

    주석 및 필요없는 라인 삭제

--- static/js/opcodes.js
+++ static/js/opcodes.js
@@ -27,24 +27,6 @@ function toggleRow(row, filename) {
         });
 }
 
-// function changeItem(rowNum) {
-//     const btnElement = document.getElementById("language-button");
-//     var enDiv = document.getElementById("EN" + rowNum);
-//     var krDiv = document.getElementById("KR" + rowNum);
-//
-//     if (btnElement.innerText == "KR") {
-//         btnElement.innerText = "EN";
-//         enDiv.style.display = "none";
-//         krDiv.style.display = "block";
-//     } else {
-//         btnElement.innerText = "KR";
-//         enDiv.style.display = "block";
-//         krDiv.style.display = "none";
-//     }
-// }
-
-
-
 
 
 
