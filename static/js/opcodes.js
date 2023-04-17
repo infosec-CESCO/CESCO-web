23b22b486ff786a69c8d3261600872099f56d78d
hiro <kyy980708@gmail.com>
2023-04-17 16:44:51 +0900

    asterisk 지우기

--- static/js/opcodes.js
+++ static/js/opcodes.js
@@ -3,31 +3,44 @@ console.log('opcodes.js loaded');
 function toggleRow(row, filename) {
     console.log('loadMarkdownFile called with filename:', filename);
     fetch(filename)
-        .then(response => response.text())
-        .then(md => {
-            const mdParser = new markdownit({ html: true });
-            const html = mdParser.render(md);
-
-            const formattedHtml = `<div class="markdown-body">${html}</div>`;
-
-            if (row.getAttribute('data-expanded') === 'true') {
-                row.nextSibling.remove();
-                row.setAttribute('data-expanded', 'false');
-            } else {
-                const newRow = document.createElement('tr');
-                newRow.classList.add('description');
-                const newCell = document.createElement('td');
-                newCell.innerHTML = formattedHtml;
-                newCell.setAttribute('colspan', row.childElementCount);
-
-                newRow.appendChild(newCell);
-                row.parentNode.insertBefore(newRow, row.nextSibling);
-                row.setAttribute('data-expanded', 'true');
+      .then(response => response.text())
+      .then(md => {
+        const mdParser = new markdownit({ html: true });
+        const html = mdParser.render(md);
+  
+        const formattedHtml = `<div class="markdown-body">${html}</div>`;
+  
+        if (row.getAttribute('data-expanded') === 'true') {
+          row.nextSibling.remove();
+          row.setAttribute('data-expanded', 'false');
+        } else {
+          const newRow = document.createElement('tr');
+          newRow.classList.add('description');
+          const newCell = document.createElement('td');
+          newCell.innerHTML = formattedHtml;
+          newCell.setAttribute('colspan', row.childElementCount);
+  
+          // Hide cells with '*'
+          const cells = newCell.querySelectorAll('td');
+          cells.forEach(cell => {
+            if (cell.textContent.trim() === '*') {
+              cell.style.visibility = 'hidden';
+              cell.classList.add('no-border');
             }
-        });
-}
+          });
 
-
-
-
-
+          const headCells = newCell.querySelectorAll('th');
+          headCells.forEach(cell => {
+            if (cell.textContent.trim() === '*') {
+              cell.style.visibility = 'hidden';
+              cell.classList.add('no-border');
+            }
+          });
+          
+  
+          newRow.appendChild(newCell);
+          row.parentNode.insertBefore(newRow, row.nextSibling);
+          row.setAttribute('data-expanded', 'true');
+        }
+      });
+  }
