319236693e73d566249c58636a505794d08812a6
Jaesun Park <41979078+ppakjae@users.noreply.github.com>
2023-04-18 10:15:53 +0900

    분석 결과 취약점 상세 페이지 링크 추가 및 opcode 페이지 width 조정

--- static/js/opcodes.js
+++ static/js/opcodes.js
@@ -3,44 +3,34 @@ console.log('opcodes.js loaded');
 function toggleRow(row, filename) {
     console.log('loadMarkdownFile called with filename:', filename);
     fetch(filename)
-      .then(response => response.text())
-      .then(md => {
-        const mdParser = new markdownit({ html: true });
-        const html = mdParser.render(md);
-  
-        const formattedHtml = `<div class="markdown-body">${html}</div>`;
-  
-        if (row.getAttribute('data-expanded') === 'true') {
-          row.nextSibling.remove();
-          row.setAttribute('data-expanded', 'false');
-        } else {
-          const newRow = document.createElement('tr');
-          newRow.classList.add('description');
-          const newCell = document.createElement('td');
-          newCell.innerHTML = formattedHtml;
-          newCell.setAttribute('colspan', row.childElementCount);
-  
-          // Hide cells with '*'
-          const cells = newCell.querySelectorAll('td');
-          cells.forEach(cell => {
-            if (cell.textContent.trim() === '*') {
-              cell.style.visibility = 'hidden';
-              cell.classList.add('no-border');
-            }
-          });
+        .then(response => response.text())
+        .then(md => {
+            const mdParser = new markdownit({html: true});
+            const html = mdParser.render(md);
 
-          const headCells = newCell.querySelectorAll('th');
-          headCells.forEach(cell => {
-            if (cell.textContent.trim() === '*') {
-              cell.style.visibility = 'hidden';
-              cell.classList.add('no-border');
+            const formattedHtml = `<div class="markdown-body">${html}</div>`;
+
+            if (row.getAttribute('data-expanded') === 'true') {
+                row.nextSibling.remove();
+                row.setAttribute('data-expanded', 'false');
+            } else {
+                const newRow = document.createElement('tr');
+                newRow.classList.add('description');
+                const newCell = document.createElement('td');
+                newCell.innerHTML = formattedHtml;
+                newCell.setAttribute('colspan', row.childElementCount);
+
+                const cells = newCell.querySelectorAll('td, th');
+                cells.forEach(cell => {
+                    if (cell.textContent.trim() === '*') {
+                        cell.style.visibility = 'hidden';
+                        cell.classList.add('no-border');
+                    }
+                });
+
+                newRow.appendChild(newCell);
+                row.parentNode.insertBefore(newRow, row.nextSibling);
+                row.setAttribute('data-expanded', 'true');
             }
-          });
-          
-  
-          newRow.appendChild(newCell);
-          row.parentNode.insertBefore(newRow, row.nextSibling);
-          row.setAttribute('data-expanded', 'true');
-        }
-      });
-  }
+        });
+}
